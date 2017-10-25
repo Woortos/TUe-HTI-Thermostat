20519ea9b0882c67056db63cfb49a04daa1ab967
Stefan <stefan2kwant@hotmail.com>
2017-10-25 16:26:15 +0200

    main screen now shows current and target temp

--- DemoThermostat/app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
+++ DemoThermostat/app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
@@ -12,10 +12,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.thermostatapp.util.HeatingSystem;
+import org.thermostatapp.util.WeekProgram;
 
 public class ThermostatActivity extends Activity {
 
     int vtemp = 22;
+    String overriddenTemp, tempCurrent;
+    TextView currentTemp;
     TextView temp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +35,22 @@ public class ThermostatActivity extends Activity {
         temp.setText(vtemp + " \u2103");
         Button weekOverview = (Button) findViewById(R.id.week_overview);
 
+        currentTemp = (TextView) findViewById(R.id.currenttemp);
+
+
+
         weekOverview.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), WeekOverview.class);
                 startActivity(intent);
+
             }
-        });
+    });
+
+
+
 
         Button testingWS = (Button) findViewById(R.id.testing_ws);
 
@@ -56,14 +67,62 @@ public class ThermostatActivity extends Activity {
             @Override
             public void onClick(View view) {
                 vtemp++;
-                temp.setText(vtemp + " \u2103");
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            HeatingSystem.put("targetTemperature", Integer.toString(vtemp));
+                            overriddenTemp = HeatingSystem.get("targetTemperature");
+                            temp.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    temp.setText(overriddenTemp);
+                                }
+                            });
+                            tempCurrent = HeatingSystem.get("currentTemperature");
+                            currentTemp.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    currentTemp.setText(tempCurrent);
+                                }
+                            });
+                        } catch (Exception e) {
+                            System.err.println("Error from getdata " + e);
+                        }
+                    }
+                }).start();
             }
         });
         bMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 vtemp--;
-                temp.setText(vtemp + " \u2103");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            HeatingSystem.put("targetTemperature", Integer.toString(vtemp));
+                            overriddenTemp = HeatingSystem.get("targetTemperature");
+                            temp.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    temp.setText(overriddenTemp);
+                                }
+                            });
+                            tempCurrent = HeatingSystem.get("currentTemperature");
+                            currentTemp.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    currentTemp.setText(tempCurrent);
+                                }
+                            });
+                        } catch (Exception e) {
+                            System.err.println("Error from getdata " + e);
+                        }
+                    }
+                }).start();
             }
         });
     }
