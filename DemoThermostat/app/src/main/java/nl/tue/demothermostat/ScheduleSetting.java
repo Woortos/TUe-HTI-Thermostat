52fe3e703c3be646f7f2205ad6e24a7706470a68
Jongelie <w.p.jongelie@student.tue.nl>
2017-10-27 18:50:38 +0200

    weekschedule in wording

---
+++ DemoThermostat/app/src/main/java/nl/tue/demothermostat/ScheduleSetting.java
@@ -0,0 +1,14 @@
+package nl.tue.demothermostat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScheduleSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_setting);
+
+    }
+}
